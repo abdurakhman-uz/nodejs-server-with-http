eae814dcaefa18784c81d018398f349482438e81
abdurakhman-uz <rockbizzy667@yandex.kz>
2023-02-18 18:51:54 +0500

    codes optimized and server successfully running

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
-const { PassHash, PassCheck, hashUser } = require('./utils/pass.js');
+const Express = require('./lib/express')
 const {read, write} = require('./utils/read.js');
-const { uuid } = require('./utils/uuid.js')
+const {uuid} = require('./utils/uuid.js')
+const {PassHash, PassCheck, hashUser} = require('./utils/pass.js');
 
 const server = http.createServer();
 const PORT = process.env.PORT || 3001;
@@ -9,207 +10,133 @@ const headers = {
     'Content-Type': "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "OPTIONS, GET, POST, PUT, DELETE",
-    "Access-Control-Max-Age": 2592000, // 30 days
+    "Access-Control-Max-Age": 2592000,
+    // 30 days
     /** add other headers as per requirement */
-  };
+};
 
 server.on('request', (req, res) => {
-    const url = req.url
-    const method = req.method
-    id = url.split('/')[2];
-    console.log({
-        "url": url,
-        "method": method
-    });
+    const app = new Express(req, res)
+    id = req.url.split('/')[2];
+    // const url = req.url
+    // id = url.split('/')[2];
+    // const method = req.method
+    // console.log({"url": url, "method": method});
 
-    if (method === 'OPTIONS') {
-        res.writeHead(200, headers);
-        res.end();
-        return;
-    }
 
-    if (url === '/products') {
-        if (method === 'GET')
-        {
-            let products = read("products")
+    app.options('*', (req, res) => {
+        res.json()
+        return
+    })
 
-            res.writeHead(200, headers)
-            res.end(JSON.stringify({msg: products}))
-        }
+    app.get('/products', (req, res) => {
+        let products = read('products')
+        res.json({msg: products})
+    })
 
-        if (method === 'POST')
-        {
-            req.on("data", chunk => {
-                let data = JSON.parse(chunk);
-                
-                let products = read("products")
-    
-                products.push({
-                    id: uuid(),
-                    ...data
-                })
-                write("products", products)
-                res.writeHead(201, headers)
-                res.end(JSON.stringify({msg: "Product Successfully added"}))
-    
-            })
-        }
-    }
+    app.post('/products', async (req, res) => {
+        let products = read("products")
+        let data = await(req.body)
+        products.push({
+            id: uuid(),
+            ... data
+        })
+        write("products", products)
+        res.json({msg: "Product Successfully added"})
+    })
 
-    if (url === `/products/${id}`) {
+    app.put(`/product/${id}`, async (req, res) => {
+        let {name, desc, price} = await(req.body)
+        let products = read("products")
+        let foundedProduct = products.find((p) => p.id === id)
 
-        if (method === 'PUT')
-        {
-            req.on("data", chunk => {
-                let newProduct = JSON.parse(chunk)
-                let { name, desc, price } = newProduct
-    
-               let products =  read("products")
-    
-               let foundedProduct = products.find((p) => p.id === id)
-    
-               if(!foundedProduct){
-                res.writeHead(404, headers)
-                return res.end(JSON.stringify({
-                    msg: 'Product not found!'
-                }))
-               }
-    
-    
-               products.forEach((product, idx) => {
-                if(product.id === id){
-                    product.name = name ? name : product.title
-                    product.desc = desc ? desc : product.desc
-                    product.price = price ? price : product.price
-                }
-               })
-    
-               write("products", products)
-    
-               res.writeHead(200, headers)
-               res.end(JSON.stringify({
-                msg: "Product Updated!"
-               }))
-    
-            })
-        }
-
-        if (method === 'DELETE')
-        {
-            console.log(id = url.split('/')[2]);
-            let products =  read("products")
-
-           let foundedProduct = products.find((p) => p.id === id)
-
-           if(!foundedProduct){
+        if (! foundedProduct) {
             res.writeHead(404, headers)
-            return res.end(JSON.stringify({
-                msg: 'Product not found!'
-            }))
-           }
+            return res.end(JSON.stringify({msg: 'Product not found!'}))
+        }
 
-           products.forEach((product, idx) => {
-            if(product.id === id){
+
+        products.forEach((product, idx) => {
+            if (product.id === id) {
+                product.name = name ? name : product.name
+                product.desc = desc ? desc : product.desc
+                product.price = price ? price : product.price
+            }
+        })
+        write("products", products)
+        res.json({msg: "Product Updated!"})
+    })
+
+    app.delete(`/product/${id}`, async (req, res) => {
+        let products = read("products")
+        let foundedProduct = products.find((p) => p.id === id)
+
+        if (! foundedProduct) {
+            return res.json({msg: 'Product not found!'})
+        }
+
+        products.forEach((product, idx) => {
+            if (product.id === id) {
                 products.splice(idx, 1)
             }
-           })
+        })
 
-           write("products", products)
+        write("products", products)
+        res.json({msg: "Product Deleted!"})
+    })
 
-           res.writeHead(200, headers)
-           res.end(JSON.stringify({
-            msg: "Product Deleted!"
-           }))
+    app.post('/auth/login', async (req, res) => {
+        let {email, password} = await(req.body)
+        let foundedUser = read("users").find(user => user.email === email)
+        if (foundedUser) {
+            let psw = PassCheck(password, foundedUser.password)
+            if (psw) { // let userToken = hashUser(`${foundedUser}`)
+                let userToken = foundedUser.password
+                return res.json({msg: 'Success!', token: userToken})
+            }
 
+            return res.json({msg: 'Password error!'})
         }
-    }
 
-    if(url === '/auth/register'){
-        req.on("data", chunk => {
-            let { email, username, password, gender } = JSON.parse(chunk)
+        res.writeHead(404, headers)
+        res.json({msg: 'User not found!'})
+    })
 
-           let psw =  PassHash(password);
+    app.post('/auth/register', async (req, res) => {
+        let {email, username, password, gender} = await(req.body)
+        let psw = PassHash(password);
 
-           let users = read("users")
+        let users = read("users")
 
-           let foundedUser = users.find(user => user.email == email)
+        let foundedUser = users.find(user => user.email == email)
 
-           if(foundedUser){
-            res.writeHead(200, headers)
-            return  res.end(JSON.stringify({
-                msg: 'Email already exists!!!'
-            }))
-           }
-           users.push({
+        if (foundedUser) {
+            return res.json({msg: 'Email already exists!!!'})
+        }
+        users.push({
             id: uuid(),
             email,
             username,
             gender,
-            password: psw,
-            })
-
-            write("users", users)
-
-            res.writeHead(201, headers)
-            res.end(JSON.stringify({
-                msg: 'User registrated!'
-            }))
-        })
-    }
-
-    if(url === '/auth/login'){
-        req.on("data", chunk => {
-            let { email, password } = JSON.parse(chunk)
-
-            let foundedUser = read("users").find(user => user.email === email)
-            if(foundedUser){
-                let psw = PassCheck(password, foundedUser.password)
-                if(psw){
-                    // let userToken = hashUser(`${foundedUser}`)
-                    let userToken = foundedUser.password
-                    res.writeHead(200, headers)
-                    return res.end(JSON.stringify({
-                        msg: 'Success!',
-                        token: userToken
-                    }))
-                }
-                
-                res.writeHead(200, headers)
-                return res.end(JSON.stringify({
-                    msg: 'Password error!'
-                }))
-            }
-
-            res.writeHead(404, options)
-            res.end(JSON.stringify({
-                msg: 'User not found!'
-            }))
-
+            password: psw
         })
 
-    }
+        write("users", users)
 
-    if(url === '/auth/check'){
-        req.on("data", chunk => {
-            let { token } = JSON.parse(chunk)
+        res.json({msg: 'User successfully registrated!'})
+    })
 
-            let foundedUser = read("users").find(user => user.password === token)
-            if(foundedUser){
-                // // let userToken = hashUser(`${foundedUser}`)
-                let userToken = foundedUser.password
-                res.writeHead(200, headers)
-                return res.end(JSON.stringify({
-                    msg: true,
-                    token: userToken
-                }))
-            }
-          
-            res.writeHead(200, headers)
-            res.end(JSON.stringify({
-                msg: false
-            }))
-        })
-    }
+    app.post('/auth/check', async (req, res) => {
+        let {token} = await(req.body)
+        let foundedUser = read("users").find(user => user.password === token)
+        if (foundedUser) { // // let userToken = hashUser(`${foundedUser}`)
+            let userToken = foundedUser.password
+            return res.json({msg: true, token: userToken})
+        }
+
+        res.json({msg: false})
+    })
+
 })
 
 server.listen(PORT, () => {
